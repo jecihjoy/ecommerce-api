6ffe38b812695ec27433343826d3290f2ad3d777
jecihjoy <jecihjoy@gmail.com>
2020-05-08 22:33:22 +0300

    Added initial data for product entity

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into
+  products (product_name,product_description,price,company_price,quantity,image_one,image_two, image_three,image_four,product_id)
+values
+  ("book", "becoming", 50, 60, 1, "img1", "img2","img3","img4",3),
+  ( "book", "becoming", 50, 60, 1, "img1", "img2", "img3", "img4", 4);
