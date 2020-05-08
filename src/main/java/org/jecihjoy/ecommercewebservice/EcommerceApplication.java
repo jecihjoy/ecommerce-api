bd6a666daeafacb5b6ba7b57f9e9411fc2078a6e
jecihjoy <jecihjoy@gmail.com>
2020-05-08 22:48:03 +0300

    Configured mysql as data source using configuration class

--- src/main/java/org/jecihjoy/ecommercewebservice/EcommerceApplication.java
+++ src/main/java/org/jecihjoy/ecommercewebservice/EcommerceApplication.java
@@ -6,12 +6,28 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class EcommerceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EcommerceApplication.class, args);
+	}
+
+	@Bean
+	public DataSource dataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+
+		dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+		dataSource.setUsername("root");
+		dataSource.setPassword("jess@2020");
+		dataSource.setUrl(
+				"jdbc:mysql://localhost:3306/ecommerce?createDatabaseIfNotExist=true");
+
+		return dataSource;
 	}
 
 	@Bean
