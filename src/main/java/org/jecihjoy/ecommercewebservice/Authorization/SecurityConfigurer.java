e5159623c27ece7b09f7dfb217d68afba098e735
jecihjoy <jecihjoy@gmail.com>
2020-06-26 09:46:37 +0300

    Added jwt auth filter to intercept API requests

--- src/main/java/org/jecihjoy/ecommercewebservice/Authorization/SecurityConfigurer.java
+++ src/main/java/org/jecihjoy/ecommercewebservice/Authorization/SecurityConfigurer.java
@@ -5,16 +5,22 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private AdminUserDetailsService adminUserDetailsService;
+
+    @Autowired
+    private JwtRequestFilter requestFilter;
 
     @Override
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -23,7 +29,16 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests().antMatchers("/authenticate").permitAll().anyRequest().authenticated();
+        http.
+                sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).
+                and().anonymous().disable();
+       http.
+               addFilterBefore(requestFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/authenticate");
     }
 
     @Override
